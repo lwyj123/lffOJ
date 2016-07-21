65e4750a279fe2715a510147e0f564bb08b41ae4
ScuAndy <931163490@qq.com>
2016-07-21 15:41:09 +0800

    Database Creation

---
+++ lffOJ.sql
@@ -0,0 +1,27 @@
+DROP TABLE Users;
+DROP TABLE Trainings;
+DROP TABLE Submission;
+create table Users (
+	Id int(10) primary key not null AUTO_INCREMENT,
+    Email varchar(20) ,
+    IsAdmin int(1) default 0,
+    Password varchar(20),
+    Nickname varchar(20),
+    RegisterDate datetime
+
+	);
+create table Trainings (
+	Id int(10) primary key not null AUTO_INCREMENT,
+	Title varchar(20),
+	Description varchar(200)
+	);
+create table Submission (
+	Id int(10) primary key not null AUTO_INCREMENT,
+	UserId int(10),
+	TrainingId int(10),
+	Status varchar(10),
+	ExcuteTime int(6),
+	MemoryUsed int(10), 
+	FOREIGN KEY(UserId)REFERENCES Users(Id),
+	FOREIGN KEY(TrainingId)REFERENCES Trainings(Id)
+	)
