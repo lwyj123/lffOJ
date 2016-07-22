59ade9d9bad7674401a459ec61754d13ca529665
CatFish <476209042@qq.com>
2016-07-22 16:22:00 +0800

    complete getTrainingList

---
+++ server/component/getTrainingList.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ 测试{
+ * "function":GetTrainingList,
+ * "Limit":20
+ * }
+ */
+
+
+function getTrainingList($json)
+{
+    require_once __DIR__ . "\\..\\lib\\medoo.php";
+
+    $database = new medoo([
+        // 必须配置项
+        'database_type' => 'mysql',
+        'database_name' => 'lffOJ',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => 'fuck',
+        'charset' => 'utf8',
+
+        // 可选参数
+        'port' => 3306,
+    ]);
+    $jsonArr = json_decode($json,true);
+    $Limit = $jsonArr['Limit'];
+
+    $data = $database->select("trainings",[
+        "Title",
+        "Description"
+    ],[
+        "LIMIT" => $Limit
+        ]
+    );
+    $res = array();
+    $res['content'] = $data;
+    return json_encode($res);
+}
